628dd7f3923b7dd8dbf87613808bbc3f66948a8d
c00b3r <sanuyOKmaklaren0>
2024-10-28 21:27:28 +0500

    init: start vk task

---
+++ src/components/RepositoryItem/RepositoryItem.tsx
@@ -0,0 +1,3 @@
+export default function RepositoryItem() {
+  return <div>RepositoryItem</div>;
+}
---
+++ src/pages/RepositoryListPage.tsx
@@ -0,0 +1,3 @@
+export default function ListOfRepositories() {
+  return <div>ListOfRepositories</div>;
+}
