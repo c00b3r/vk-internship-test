7369258a4d3294909222efba0f8354bbed6e2178
c00b3r <sanuyOKmaklaren0>
2024-10-28 23:39:13 +0500

    feat: add auth token into .env file for work with github API

--- src/pages/RepositoryListPage.tsx
+++ src/pages/RepositoryListPage.tsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+
 export default function ListOfRepositories() {
-  return <div>ListOfRepositories</div>;
+  const [dataOfRepos, setDataOfRepos] = useState([]);
+
+  useEffect(() => {
+    const token = import.meta.env.VITE_TOKEN;
+    async function getReposData() {
+      try {
+        const response = await fetch("https://api.github.com/search/repositories?q=javascript", {
+          headers: {
+            Accept: "application/vnd.github.v3+json",
+            Authorization: `token ${token}`,
+          },
+        });
+        const data = await response.json();
+        setDataOfRepos(data.items);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getReposData();
+  }, []);
+
+  console.log(dataOfRepos);
+  return <div>lol</div>;
 }
